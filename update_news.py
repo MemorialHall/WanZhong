79716a252d61334a6548d9f23f17f130f32ecb79
xfangfang <2553041586@qq.com>
2020-12-23 15:34:28 +0800

    py2 to py3

--- update_news.py
+++ update_news.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+#!/usr/bin python3
+
 import requests
 import json
 
@@ -13,21 +11,20 @@ page = 1
 def getJson():
     global page
     headers = {
-    'Accept':'application/json'
+    'Accept':'application/json',
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
     }
     res = requests.get(URL+query+str(page),headers=headers)
     j = json.loads(res.text)
     totalPage = j['total_pages']
     page = j['page']
-    f = open("page/"+str(page)+".html","w")
-    f.write(res.text)
-    f.close()
+    with open("page/"+str(page)+".html","w") as f:
+        f.write(res.text)
 
     for i in j['notes']:
-        f = open("page/"+i['slug']+".html","w")
-        res = requests.get("http://www.jianshu.com/p/"+i['slug'])
-        f.write(res.text)
-        f.close()
+        with open("page/"+i['slug']+".html","w") as f:
+            res = requests.get("http://www.jianshu.com/p/"+i['slug'],headers=headers)
+            f.write(res.text)
 
     if page < totalPage:
         page = page + 1
